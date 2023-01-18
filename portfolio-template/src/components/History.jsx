6a332be1fbb447061ace8493f5324ccf43a4778c
Marmamello <samuvirtanen96@gmail.com>
2023-01-18 20:16:57 +0200

    asd

--- portfolio-template/src/components/History.jsx
+++ portfolio-template/src/components/History.jsx
@@ -1,11 +1,15 @@
 export function History() {
   return (
-    <div className="sidebar  w-full bg-orange-400 h-[960px]">
-      <div className="sidebar__content">
-        <h1 className="sidebar__title">John Doe</h1>
-        <p className="sidebar__description">
-          I'm a web developer based in New York City.
-        </p>
+
+    <div className="sidebar  w-full bg-orange-400 h-[960px] flex items-start ">
+      <div className="sidebar__content w-full flex justify-center gap-10 pt-20">
+        <div class="leftcontent bg-blue-500 w-[550px]">
+          <h2 class="text-4xl font-medium">Experience</h2>
+          <h1 class="text-xl font-medium">2019 - Present Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde, laudantium provident. Quasi sunt odit voluptates. Totam labore animi dolore, assumenda velit repellat veritatis suscipit blanditiis ipsam minima nisi rem numquam?</h1>
+        </div>
+        <div className="rightcontent bg-green-700 w-[550px]">
+          <h1 class="text-xl font-medium">2019 - Present Lorem ipsum dolor, sasdasdasdum provident. Quasi sunt odit voluptates. Totam labore animi dolore, assumenda velit repellat veritatis suscipit blanditiis ipsam minima nisi rem numquam?</h1>
+        </div>
       </div>
     </div>
   );
